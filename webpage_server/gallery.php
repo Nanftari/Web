5292a5176b1dcf5fd483c3c7b56ab41a3ddd6f2e
Nanftari <97348518+Nanftari@users.noreply.github.com>
2023-06-19 17:57:27 +0800

    Fixed an bug on formatting

--- webpage_server/gallery.php
+++ webpage_server/gallery.php
@@ -32,41 +32,42 @@
 				
 				<h2 id="gallery_0" class="scriptum" style="font-size:300%;text-align:center">画廊</h2>
 				<style>
+				<style>
 					.images_div image {
 						max-width: 100%;
 						object-fit: cover;
 					}
 					.images_div figcaption {
-						padding: 0.3em 0.8em;
+						padding: 0.2em 0.4em;
+						font-size: 0.5rem;
 						background-color: rgba(0, 0, 0, 0.5);
 						color: white;
 						text-align: right;
 					}
 					.images_div {
-							display: grid;
-							grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-							grid-auto-rows: 1fr;
-							grid-gap: 1em;
-							grid-auto-flow: dense;
-						}
+						display: grid;
+						grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+						grid-auto-rows: 1fr;
+						grid-gap: 0.2em;
+						grid-auto-flow: dense;
+					}
 					.images_div > figure {
-							display: flex;
-							flex-direction: column;
-							max-width: initial;
-						}
+						display: flex;
+						flex-direction: column;
+						max-width: initial;
+					}
 					.images_div image {
-							max-width: 100%;
-							object-fit: cover;
-							flex: 1;
-						}
-					.images_div .bigImages{
-							grid-row: span 2;
-							grid-column: span 2;
-						}
+						max-width: 100%;
+						object-fit: cover;
+						flex: 1;
+					}
+					#images_div_id .bigImages{
+						grid-row: span 2;
+						grid-column: span 2;
 					}
 				</style>
 				<hr />
-				<div class='images_div'>
+				<div id='images_div_id' class='images_div'>
 				
 					<figure>
 						<image loading="lazy" src='../Images/blog/Ringtonus.jpg' alt='Author'>
@@ -79,33 +80,33 @@
 					</figure>
 					
 					<figure>
-						<image loading="lazy" class='bigImages' src='../Images/index/origin/2023-05-12_20.39.09_2.png' alt='png1'>
-						<figcaption></figcaption>
+						<image loading="lazy" class='bigImages' src='../Images/index/2023-05-13_11.17.57.jpg' alt='screeshot1'>
+						<figcaption>伺服器截圖</figcaption>
 					</figure>
 					
 					<figure>
-						<image loading="lazy" class='bigImages' src='../Images/index/origin/2023-05-13_11.16.03.png' alt='png2'>
-						<figcaption></figcaption>
+						<image loading="lazy" class='bigImages' src='../Images/index/2023-05-13_11.16.03.jpg' alt='screeshot2'>
+						<figcaption>伺服器截圖</figcaption>
 					</figure>
 					
 					<figure>
-						<image loading="lazy" class='bigImages' src='../Images/index/origin/2023-05-13_11.17.57.png' alt='png3'>
-						<figcaption></figcaption>
+						<image loading="lazy" class='bigImages' src='../Images/index/2023-05-13_11.32.23.jpg' alt='screeshot3'>
+						<figcaption>伺服器截圖</figcaption>
 					</figure>
 					
 					<figure>
-						<image loading="lazy" class='bigImages' src='../Images/index/origin/2023-05-13_11.32.23.png' alt='png4'>
-						<figcaption></figcaption>
+						<image loading="lazy" class='bigImages' src='../Images/index/2023-05-13_12.17.28.jpg' alt='screeshot4'>
+						<figcaption>伺服器截圖</figcaption>
 					</figure>
 					
 					<figure>
-						<image loading="lazy" class='bigImages' src='../Images/index/origin/2023-05-13_12.17.28.png' alt='png5'>
-						<figcaption></figcaption>
+						<image loading="lazy" class='bigImages' src='../Images/index/2023-05-13_13.19.17.jpg' alt='screeshot5'>
+						<figcaption>伺服器截圖</figcaption>
 					</figure>
 					
 					<figure>
-						<image loading="lazy" class='bigImages' src='../Images/index/origin/2023-05-13_13.19.17.png' alt='png6'>
-						<figcaption></figcaption>
+						<image loading="lazy" class='bigImages' src='../Images/index/2023-05-12_20.39.09_2.jpg' alt='screeshot6'>
+						<figcaption>伺服器截圖</figcaption>
 					</figure>
 					
 
